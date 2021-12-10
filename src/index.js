6f6107b031549062e0e4e7823b00dd5f6385f47c
Ohmcode <email@jasperfu.com>
2021-12-10 12:07:47 -0500

    mvp

--- src/index.js
+++ src/index.js
@@ -55,15 +55,18 @@ const loadPage1 = () =>
 
 
     const p1c2textbox =pageLoad.createNewElement('div', 'textbox', 'p1c2textbox');
+    p1c2textbox.textContent = "Nebulas are very cool looking! "
     const p1c3textbox =pageLoad.createNewElement('div', 'textbox', 'p1c3textbox');
     p1c3textbox.textContent = "contact information:"
     const p1c3iframe =document.createElement('iframe');
+    //embed google maps
     const googleMapsLink = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63833.231980735676!2d73.14289741361111!3d-0.6301752026039324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x24b599bfaafb7bbd%3A0x414509e181956289!2sMaldives!5e0!3m2!1sen!2sus!4v1639096087116!5m2!1sen!2sus`
     p1c3iframe.setAttribute('src', googleMapsLink)
     p1c3iframe.setAttribute('width', 400)
     p1c3iframe.setAttribute('height',300)
     p1c3iframe.setAttribute('loading','lazy')
     p1c3iframe.setAttribute('style','border:0')
+    //end google iframe
         pageContent1.appendChild(p1c1textbox)
         pageContent1.appendChild(p1c1ImgHolder)
         pageContent2.appendChild(p1c2textbox)
@@ -82,10 +85,11 @@ const loadPage2 = ()=>{
     const pageContent1 = pageLoad.createNewElement('div','content','pageContent1');
     const pageContent2 = pageLoad.createNewElement('div','content','pageContent2');
     const pageContent3 = pageLoad.createNewElement('div','content','pageContent3');
+    const page2ImageHolder = pageLoad.createNewElement('img','image','nebula3');
+    page2ImageHolder.src = nebula3;
 
 
-
-
+    pageContent1.appendChild(page2ImageHolder)
     page1.appendChild(pageContent1)
     page1.appendChild(pageContent2)
     page1.appendChild(pageContent3)
@@ -117,6 +121,7 @@ const headerBorderReset =()=>{
 
 headerItems.forEach(((currentValue,index) =>{
     let element = document.getElementById(currentValue.id)
+    element.textContent = element.id
     element.addEventListener('click',()=>{
         headerBorderReset();
         element.style.borderBottom = 'hidden';
